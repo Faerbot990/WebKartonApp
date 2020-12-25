9961aed4a2f9607d3b975dad23ca10c163e1acdf
Faerbot990 <74986234+Faerbot990@users.noreply.github.com>
2020-12-25 16:09:02 +0200

    Add files via upload

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -52,12 +52,14 @@ $(document).ready(function () {
 		data["username"] = $("[name='username']").val();
 		data["password"] = $("[name='password']").val();
 
+		var myJSON = JSON.stringify(data);
+
 
 		$.ajax({
 			url: url,
 			dataType: 'json',
 			type: 'post',
-			data: data,
+			data: myJSON,
 			success: function(){
 				
 			},
