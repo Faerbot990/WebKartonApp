d0a5f48548a110f38d17d11ce9581f4aa3cd6403
Vladimir Shevchuck <67117750+Reciepthack@users.noreply.github.com>
2020-12-30 13:14:16 +0200

    add bcrypt

--- src/main/resources/db/migration/V2__Add_usr.sql
+++ src/main/resources/db/migration/V2__Add_usr.sql
@@ -1,4 +1,4 @@
 INSERT INTO usr (id,username,password)
-VALUES (1,'admin','admin');
+VALUES (1,'admin','$2y$12$WLzZMcYvvz.vjSJGKw/2dO20u1ZxQK/wS/J3XpB7low3x3ZRnr2H.');
 INSERT INTO user_role (user_id, roles)
 VALUES (1, 'ROLE_ADMIN');
