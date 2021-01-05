432a9bf70edef615346c1f57202f1f08d53d6019
reciepthack <faerbot123>
2021-01-05 16:34:39 +0200

    autority admin controller

--- src/main/java/com/example/WebKartonApp/controller/AdminController.java
+++ src/main/java/com/example/WebKartonApp/controller/AdminController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -28,6 +29,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
+@PreAuthorize("hasRole('ROLE_ADMIN')")
 @RequestMapping("/api/v1/admin")
 @Slf4j
 public class AdminController {
