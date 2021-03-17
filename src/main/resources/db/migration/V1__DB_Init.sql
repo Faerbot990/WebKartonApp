982823e3b877db1635f440fc95ed904c083f328c
world123990@gmail.com <faerbot-123>
2021-03-17 16:29:56 +0200

    update flyway

--- src/main/resources/db/migration/V1__DB_Init.sql
+++ src/main/resources/db/migration/V1__DB_Init.sql
@@ -1,5 +1,4 @@
-
-create sequence hibernate_sequence start 1 increment 1
+create sequence hibernate_sequence start 1 increment 1;
 
 create table category
 (
@@ -10,7 +9,7 @@ create table category
     name_sub_slug varchar(255),
     category_slug varchar(255),
     primary key (slug)
-)
+);
 create table news
 (
     id          int8 not null,
@@ -18,7 +17,7 @@ create table news
     information varchar(255),
     title       varchar(255),
     primary key (id)
-)
+);
 create table product
 (
     slug                varchar(255) not null,
@@ -30,30 +29,30 @@ create table product
     quantity            varchar(255),
     category_slug       varchar(255),
     primary key (slug)
-)
+);
 create table user_role
 (
     user_id int8 not null,
     roles   varchar(255)
-)
+);
 create table usr
 (
     id       int8 not null,
     password varchar(255),
     username varchar(255),
     primary key (id)
-)
+);
 create table usr_product_list
 (
     user_id           int8         not null,
     product_list_slug varchar(255) not null
-)
-alter table if exists category add constraint FK5h87fxn3a4hqktk64keodev71 foreign key (category_slug) references category
+);
+alter table if exists category add constraint FK5h87fxn3a4hqktk64keodev71 foreign key (category_slug) references category;
 
-alter table if exists product add constraint FKgxuoidemfiurovm8ktg84hett foreign key (category_slug) references category
+alter table if exists product add constraint FKgxuoidemfiurovm8ktg84hett foreign key (category_slug) references category;
 
-alter table if exists user_role add constraint FKfpm8swft53ulq2hl11yplpr5 foreign key (user_id) references usr
+alter table if exists user_role add constraint FKfpm8swft53ulq2hl11yplpr5 foreign key (user_id) references usr;
 
-alter table if exists usr_product_list add constraint FKonawyj3mbv57ribnkg3nd13g2 foreign key (product_list_slug) references product
+alter table if exists usr_product_list add constraint FKonawyj3mbv57ribnkg3nd13g2 foreign key (product_list_slug) references product;
 
-alter table if exists usr_product_list add constraint FKp5vrlq5f8efd6ecfxmhcm0px5 foreign key (user_id) references usr
+alter table if exists usr_product_list add constraint FKp5vrlq5f8efd6ecfxmhcm0px5 foreign key (user_id) references usr;
