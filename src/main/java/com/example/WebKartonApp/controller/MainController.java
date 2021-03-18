a2081229e8ba7c44016efa87208cc6e95d1147b5
world123990@gmail.com <faerbot-123>
2021-03-18 11:01:19 +0200

    update flyway

--- src/main/java/com/example/WebKartonApp/controller/MainController.java
+++ src/main/java/com/example/WebKartonApp/controller/MainController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/v1/main")
+@RequestMapping("/main")
 public class MainController {
 
     private final ProductService productService;
