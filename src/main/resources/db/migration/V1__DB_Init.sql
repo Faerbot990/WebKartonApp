c12642c6aae9c248be3f84f9d05545f56a372205
world123990@gmail.com <faerbot-123>
2021-04-02 15:44:08 +0300

    Update migration

--- src/main/resources/db/migration/V1__DB_Init.sql
+++ src/main/resources/db/migration/V1__DB_Init.sql
@@ -1,37 +1,45 @@
-create sequence hibernate_sequence start 1 increment 1;
 
+create sequence hibernate_sequence start 1 increment 1;
 create table category
 (
-    slug          varchar(255) not null,
-    image         varchar(200000),
-    name          varchar(255),
-    name_sub      varchar(255),
-    name_sub_slug varchar(255),
-    category_slug varchar(255),
-    primary key (slug)
+    id         int8 not null,
+    local_date date,
+    name       varchar(255),
+    slug       varchar(255),
+    primary key (id)
 );
 create table news
 (
     id          int8 not null,
-    date        timestamp,
     filename    varchar(2000000),
     information varchar(255),
-    title       varchar(255),
     local_date  date,
+    title       varchar(255),
     primary key (id)
 );
 create table product
 (
-    slug                varchar(255) not null,
+    id                  int8   not null,
     filename            varchar(999999),
-    price               float8       not null,
+    local_date          date,
+    price               float8 not null,
     product_color       varchar(255),
     product_description varchar(2048),
     product_name        varchar(255),
     quantity            varchar(255),
-    category_slug       varchar(255),
-    local_date          date,
-    primary key (slug)
+    slug                varchar(255),
+    category_id         int8,
+    primary key (id)
+);
+create table subcategory
+(
+    id                     int8 not null,
+    image                  varchar(999999),
+    local_date             date,
+    sub_category_name      varchar(255),
+    sub_category_name_slug varchar(255),
+    category_id            int8,
+    primary key (id)
 );
 create table user_role
 (
@@ -45,17 +53,9 @@ create table usr
     username varchar(255),
     primary key (id)
 );
-create table usr_product_list
-(
-    user_id           int8         not null,
-    product_list_slug varchar(255) not null
-);
-alter table if exists category add constraint FK5h87fxn3a4hqktk64keodev71 foreign key (category_slug) references category;
-
-alter table if exists product add constraint FKgxuoidemfiurovm8ktg84hett foreign key (category_slug) references category;
-
-alter table if exists user_role add constraint FKfpm8swft53ulq2hl11yplpr5 foreign key (user_id) references usr;
-
-alter table if exists usr_product_list add constraint FKonawyj3mbv57ribnkg3nd13g2 foreign key (product_list_slug) references product;
-
-alter table if exists usr_product_list add constraint FKp5vrlq5f8efd6ecfxmhcm0px5 foreign key (user_id) references usr;
+alter table if exists product
+    add constraint FK1mtsbur82frn64de7balymq9s foreign key (category_id) references category;
+alter table if exists subcategory
+    add constraint FKe4hdbsmrx9bs9gpj1fh4mg0ku foreign key (category_id) references category;
+alter table if exists user_role
+    add constraint FKfpm8swft53ulq2hl11yplpr5 foreign key (user_id) references usr
